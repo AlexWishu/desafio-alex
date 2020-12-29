983dd325fec1905cf026cffb4187993580a1c4c5
Alexander Perez Urbina <alexander.perez.urbina55@gmail.com>
2020-12-29 11:18:56 -0300

    fontawesome ahora esta en archivo local y contactos se ve disminuido su tamaño

--- app.js
+++ app.js
@@ -7,3 +7,17 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
+
+const iconContainer = document.getElementsByClassName('icon-container');
+
+for(let i = 0; i < iconContainer.length; i++){
+    iconContainer[i].addEventListener("mouseover", () => {
+        let icon = iconContainer[i].querySelector('i');
+        icon.classList.add('font-white');
+    })
+
+    iconContainer[i].addEventListener("mouseout", () => {
+        let icon = iconContainer[i].querySelector('i');
+        icon.classList.remove('font-white');
+    })
+}
